e38349d3ebe29685ef62de155fb100f753f6cf1c
Juyounglee95 <dongry30@gmail.com>
2017-09-27 16:07:53 +0900

    commit

--- app/src/main/java/com/example/user/articlefragment/ArticleFragment.java
+++ app/src/main/java/com/example/user/articlefragment/ArticleFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by user on 2017-09-20.
@@ -12,6 +13,29 @@ import android.view.ViewGroup;
 
 public class ArticleFragment extends Fragment {
     final static String ARG_POSITION="position";
+    int mCurrentPosition= -1;
+    public void onStart(){
+        super.onStart();
+        Bundle args = getArguments();
+        if(args!= null){
+            updateArticleView(args.getInt(ARG_POSITION));
+
+        }else if(mCurrentPosition!=-1)
+        {
+            updateArticleView(mCurrentPosition);
+        }
+
+    }
+    public void updateArticleView(int position){
+        TextView article = (TextView)getActivity().findViewById(R.id.article);
+        article.setText(Ipsum.Articles[position]);
+        mCurrentPosition=position;
+    }
+    public void onSaveInstanceState(Bundle outState){
+
+        super.onSaveInstanceState(outState);
+        outState.putInt(ARG_POSITION,mCurrentPosition);
+    }
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         return inflater.inflate(R.layout.article_view, container, false);
